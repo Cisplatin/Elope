b3f4a7dd8dacac65f0ffaf3ce0df1c408af7d1e0
Xenonstory <simon.j.hajjar@gmail.com>
2015-07-13 21:13:51 -0400

    finished __eq__ and __lt__ func for Player

--- Player.py
+++ Player.py
@@ -1,17 +1,22 @@
 class Player:
 
 	def __init__(self, score):
-		this.score = score
+		"""
+		__ init__(self, score) creates and returns new player with 
+		the given score.
+		"""
+		self.score = score
 
 	def __eq__(self, other):
 		"""
-		__eq__(self, other) returns true if the scores of
-		the two given players are equal, false otherwise.
+		__eq__(self, other) returns true if the scores of the two given
+		players are equal, false otherwise.
 		"""
 		return self.score == other.score
 
-
-
-a = Player()
-b = Player()
-a == b
+	def __lt__(self, other):
+		"""
+		__lt__(self, other) returns true if the other score is higher
+		than the self score.
+		"""
+		return self.score < other.score
