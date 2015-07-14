7c979f2cf258c177a65cb6c5c60b905a7567dc38
dkiv <david.kivlichan@gmail.com>
2015-07-13 21:44:58 -0400

    added various player functions

--- Player.py
+++ Player.py
@@ -16,16 +16,43 @@ class Player:
 
 	def __lt__(self, other):
 		"""
-		__lt__(self, other) returns true if the other score is higher
-		than the self score.
+		__lt__(self, other) returns true if the self score is less
+		than the other score.
 		"""
 		return self.score < other.score
 
-	def score():
-		return this.score
+	def __gt__(self, other):
+		"""
+		__gt__(self, other) returns true if the self score is greater
+		than the other score.
+		"""
+		return self.score > other.score
 
+	def __le__(self, other):
+		"""
+		__le__(self, other) returns true if the self score is less than 		or equal to the other score.
+		"""
+		return self.score <= other.score
+
+	def __ge__(self,other):
+		"""
+		___ge___(self, other) returns true if the self score is greater 		than or equal to the other score. 
+		"""
+
+	def getScore(self):
+		"""
+		score(self) returns the score of the player.
+		"""
+		return self.score
+
+	def changeScore(self, change):
+		"""
+		changScore(self, change) increases the score of the player by 
+		the value given in change.
+		"""
+		self.score += change
 	
-
+	
 a = Player()
 b = Player()
 a == b
