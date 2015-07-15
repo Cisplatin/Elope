9ad1e5c098946d2e8d5e822030690258f0bdf590
Xenonstory <simon.j.hajjar@gmail.com>
2015-07-14 22:13:43 -0400

    changed tabs to spaces

--- ELO.py
+++ ELO.py
@@ -1,55 +1,56 @@
 from Player import Player
 
 class ELO:
-	
-	DEFAULT_SCORE = 1000 # The default score for new players
+    
+    DEFAULT_SCORE = 1000 # The default score for new players
 
-	def __init__(self):
-		"""
-		__init__(self) initializes an empty ELO container used to hold
-		all players involved in this specific scheme.
-		"""		
-		self.players = {}
+    def __init__(self):
+        """
+        __init__(self) initializes an empty ELO container used to hold
+        all players involved in this specific scheme.
+        """        
+        self.players = {}
+        self.game_count = 0
 
-	def add_player(self, player):
-		"""
-		add_player(self, player) adds player to the list of players
-		in the scheme. They start with the average score of 1000.
-		"""
-		if player in self.players:
-			raise Exception("There is already a player named %s." 
-					% player)
-		else:
-			self.players[player] = Player(ELO.DEFAULT_SCORE)
+    def add_player(self, player):
+        """
+        add_player(self, player) adds player to the list of players
+        in the scheme. They start with the average score of 1000.
+        """
+        if player in self.players:
+            raise Exception("There is already a player named %s." 
+                    % player)
+        else:
+            self.players[player] = Player(ELO.DEFAULT_SCORE)
 
-	def remove_player(self, player):
-		"""
-		remove_player(self, player) removes the player given from the
-		list of players in the scheme.
-		"""
-		if not player in self.players:
-			raise Exception("There is no player named %s."
-					% player)
-		else:
-			del self.players[player]
+    def remove_player(self, player):
+        """
+        remove_player(self, player) removes the player given from the
+        list of players in the scheme.
+        """
+        if not player in self.players:
+            raise Exception("There is no player named %s."
+                    % player)
+        else:
+            del self.players[player]
 
-	def number_of_players(self):
-		"""
-		number_of_players(self) returns the number of players in this
-		scheme.
-		"""
-		return len(self.players)
+    def number_of_players(self):
+        """
+        number_of_players(self) returns the number of players in this
+        scheme.
+        """
+        return len(self.players)
 
-	def average_score(self):
-		"""
-		average_score(self) returns the average score of all players in
-		the given ELO scheme.
-		"""
-		player_count = self.number_of_players()
-		if player_count == 0:
-			raise Exception("Cannot find the average score of "
-					"0 players.")
-		else:
-			total = sum([self.players[player].get_score()
-				     for player in self.players])
-			return total / self.number_of_players()
+    def average_score(self):
+        """
+        average_score(self) returns the average score of all players in
+        the given ELO scheme.
+        """
+        player_count = self.number_of_players()
+        if player_count == 0:
+            raise Exception("Cannot find the average score of "
+                    "0 players.")
+        else:
+            total = sum([self.players[player].get_score()
+                     for player in self.players])
+            return total / self.number_of_players()
--- Player.py
+++ Player.py
@@ -1,123 +1,118 @@
 class Player:
 
     MAX_DIFFERENCE = 400 # Maximum score difference for effect change
-	
+    
     THRESHHOLD_MATCHES = 30 # Minimum number of matches a player has to play
-	                        # for their k factor to be assigned other than 40
-    THRESHHOLD_SCORE = 2400 # Minimum score for players to have a kfactor change 								
+                            # for their k factor to be assigned other than 40
+    THRESHHOLD_SCORE = 2400 # Minimum score for players to have a kfactor change                                
     K_TIER1 = 40 # the kfactor for a player with less than threshhold_matches
-
     K_TIER2 = 20 # the kfactor for a player who's score has never been above
-
     K_TIER3 = 10 # the kfactor for a player who's score has been above
 
+    def __init__(self, score):
+        """
+        __ init__(self, score) creates and returns new player with 
+        the given score.
+        """
+        self.score = score
+        self.wins = 0
+        self.ties = 0
+        self.loses = 0
+        self.been_above_threshhold_score = False
 
-	
+    def __eq__(self, other):
+        """
+        __eq__(self, other) returns true if the scores of the two given
+        players are equal, false otherwise.
+        """
+        return self.score == other.score
 
-	def __init__(self, score):
-		"""
-		__ init__(self, score) creates and returns new player with 
-		the given score.
-		"""
-		self.score = score
-		self.wins = 0
-		self.ties = 0
-		self.loses = 0
-		self.been_above_threshhold_score = False
+    def __lt__(self, other):
+        """
+        __lt__(self, other) returns true if the self score is less
+        than the other score.
+        """
+        return self.score < other.score
 
-	def __eq__(self, other):
-		"""
-		__eq__(self, other) returns true if the scores of the two given
-		players are equal, false otherwise.
-		"""
-		return self.score == other.score
+    def __gt__(self, other):
+        """
+        __gt__(self, other) returns true if the self score is greater
+        than the other score.
+        """
+        return self.score > other.score
 
-	def __lt__(self, other):
-		"""
-		__lt__(self, other) returns true if the self score is less
-		than the other score.
-		"""
-		return self.score < other.score
+    def __le__(self, other):
+        """
+        __le__(self, other) returns true if the self score is less than 
+        or equal to the other score.
+        """
+        return self.score <= other.score
 
-	def __gt__(self, other):
-		"""
-		__gt__(self, other) returns true if the self score is greater
-		than the other score.
-		"""
-		return self.score > other.score
+    def __ge__(self, other):
+        """
+        ___ge___(self, other) returns true if the self score is greater 
+        than or equal to the other score. 
+        """
+        return self.score >= other.score
 
-	def __le__(self, other):
-		"""
-		__le__(self, other) returns true if the self score is less than 
-		or equal to the other score.
-		"""
-		return self.score <= other.score
+    def __iadd__(self, arg):
+        """
+        ___iadd__(self, arg) adds the arg value to the player's score.
+        """
+        if (self.score + arg >= THRESHHOLD_SCORE):
+            self.been_above_threshhold_score = True
 
-	def __ge__(self, other):
-		"""
-		___ge___(self, other) returns true if the self score is greater 
-		than or equal to the other score. 
-		"""
-		return self.score >= other.score
+        return self.score + arg
 
-	def __iadd__(self, arg):
-		"""
-		___iadd__(self, arg) adds the arg value to the player's score.
-		"""
-		if (self.score + arg >= THRESHHOLD_SCORE):
-			self.been_above_threshhold_score = True
+    def __isub__(self, arg):
+        """
+        __isub__(self, arg) subtracts the arg value from the 
+        player's score.
+        """
+        return self.score - arg
 
-		return self.score + arg
+    def get_score(self):
+        """
+        get_score(self) returns the score of the player.
+        """
+        return self.score
 
-	def __isub__(self, arg):
-		"""
-		__isub__(self, arg) subtracts the arg value from the 
-		player's score.
-		"""
-		return self.score - arg
+    def set_score(self, new_score):
+        """
+        get_score(self, new_score) sets the players score to new_score.
+        """
+        if (new_score >= THRESHHOLD_SCORE):
+            self.been_above_threshhold_score = True
+        self.score = new_score
 
-	def get_score(self):
-		"""
-		get_score(self) returns the score of the player.
-		"""
-		return self.score
+    def get_matches(self):
+        """
+        get_matches(self) returns the total number of matches played by 
+        this player over all time.
+        """
+        return self.wins + self.ties + self.loses
 
-	def set_score(self, new_score):
-		"""
-		get_score(self, new_score) sets the players score to new_score.
-		"""
-		if (new_score >= THRESHHOLD_SCORE):
-		    self.been_above_threshhold_score = True
-		self.score = new_score
-
-	def get_matches(self):
-		"""
-		get_matches(self) returns the total number of matches played by 
-		this player over all time.
-		"""
-		return self.wins + self.ties + self.loses
-
-	def expected_score_against(self, other):
-		"""
-		expected_score_against(self) returns the expected score against
-		the other player.
-		"""
-		ratio = (other.get_score() - self.score) / float(Player.MAX_DIFFERENCE)
-		return 1 / (1 + 10 ** ratio)
+    def expected_score_against(self, other):
+        """
+        expected_score_against(self) returns the expected score against
+        the other player.
+        """
+        ratio = (other.get_score() - self.score) / float(Player.MAX_DIFFERENCE)
+        return 1 / (1 + 10 ** ratio)
 
     def k_factor(self):
-	    """
+        """
         k_factor returns the development coefficient for a specific player
-		based on the following rules
-		k is 40 for a player who has played less than THRESHHOLD_GAMES
-		k is 20 for a player who's rating is under THRESHHOLD_SCORE
-		k is 10 for a player whos rating has ever been above THRESHHOLD_SCORE
-		"""
+        based on the following rules
+        k is 40 for a player who has played less than THRESHHOLD_GAMES
+        k is 20 for a player who's rating is under THRESHHOLD_SCORE
+        k is 10 for a player whos rating has ever been above THRESHHOLD_SCORE
+        """
         if (self.get_matches < THRESHHOLD_GAMES): 
-		    return K_TIER1
-		elif (self.been_above_threshhold_score):
-			return K_TIER3
-		elif return K_TIER2 	
+            return K_TIER1
+        elif (self.been_above_threshhold_score):
+            return K_TIER3
+        elif return K_TIER2        
 
 a = Player(1200)
 print a.k_factor
